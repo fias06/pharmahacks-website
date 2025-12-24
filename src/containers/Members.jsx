efb9c71c7436d5ab7ec177ca163077d9537754ae
fias06 <saifmshaikh06@gmail.com>
2025-12-24 15:24:29 -0500

    updating images

--- src/containers/Members.jsx
+++ src/containers/Members.jsx
@@ -10,7 +10,7 @@ export default class Members extends React.Component{
                 <div className='m-prez'>
                     <div className='m-committees'><span>Pharmahacks President</span></div>  
                     <div className='m-prezImg'>
-                        <Member img="public/images/members/2025_members/Onur_Gul.png" n="Onur Gul" position="President" field="U3 Mathematics/Computer Science" className = "vp-border"/>
+                        <Member img="2025_members/Onur_Gul.jpg" n="Onur Gul" position="President" field="U3 Mathematics/Computer Science" className = "vp-border"/>
                     </div>
                     {/* <div className='m-prezText'>
                         <span> "We are a group of students passionate about facilitating the transition of students from academia to industry by organizing <br/>
@@ -34,37 +34,37 @@ export default class Members extends React.Component{
                 <div>
                     <div className='m-committees'><span>Sponsorship</span></div>          
                     <div className='m-list'>
-                        <Member img="2024_members/Celine.png" n="Celine Sakkal" position=" VP Sponsorship" field="U1 Computer Science" className="vp-border"/>
-                        <Member img="2024_members/Antonin.png" n="Antonin Beranger" position="Sponsorship Coordinator" field="U2 Economics and Computer Science"className="non-vp-border"/>
-                        <Member img="2024_members/Marie.png" n="Marie Pyun" position="Sponsorship Coordinator" field="U1 Biology and Math"className="non-vp-border"/>
+                        <Member img="2025_members/Celine.jpg" n="Celine Sakkal" position=" VP Sponsorship" field="U2 Computer Science" className="vp-border"/>
+                        <Member img="2025_members/Yue_Qian_Zhang.jpg" n="Yue Qian Zhang" position="Sponsorship Coordinator" field="U1 Med-P"className="non-vp-border"/>
+                        <Member img="2025_members/Marie.jpg" n="Marie Pyun" position="Sponsorship Coordinator" field="U2 Biology and Math"className="non-vp-border"/>
                     </div>
                     <div className='m-committees'><span>Finance</span></div> 
                     <div className='m-list'>
-                    <Member img="2024_members/Ali.png" n="Ali Douba" position=" VP Finance" field="U4 Finance, Analytics, and Sustainability" className="vp-border"/>
-                    <Member img="2024_members/Shahd.png" n="Shahd Audi" position=" Finance Coordinator" field="U3 Kinesiology" className="non-vp-border"/>
+                    <Member img="2025_members/Tracy.jpg" n="Tracy Wang" position=" VP Finance" field="U3 Neuroscience" className="vp-border"/>
+                    <Member img="2025_members/Zi_Yu_Choo.jpg" n="Zi Yu Choo" position=" Finance Coordinator" field="Finance (exchange)" className="non-vp-border"/>
                     </div>
                     <div className='m-committees'><span>Logistics</span></div> 
                     <div className='m-list'>
-                    <Member img="2024_members/Tracy.png" n="Tracy Wang" position="VP Logistics" field="U2 Neuroscience" className="vp-border"/>
-                    <Member img="2024_members/Zhi.png" n="Zhi Jun Liu" position="Logistics Coordinator" field="U1 Med-P"className="non-vp-border"/>
-                    <Member img="2024_members/Tanjin.jpg" n="Tanjin Sultana" position="Logistics Coordinator" field="U2 Bioengineering"className="non-vp-border"/>
+                    <Member img="2025_members/Stephen_Blackburn.jpg" n="Stephen Blackburn" position="VP Logistics" field="U1 Finance" className="vp-border"/>
+                    <Member img="2025_members/Jackson_Yu.jpg" n="Jackson Yu" position="Logistics Coordinator" field="U3 Bioengineering"className="non-vp-border"/>
+                    <Member img="2025_members/Alina_Weng.jpg" n="Alina Weng" position="Logistics Coordinator" field="U2 Bioengineering"className="non-vp-border"/>
                     </div>
                     <div className='m-committees'><span>Communications</span></div> 
                     <div className='m-list'>
-                    <Member img="2024_members/Cindy.png" n="Cindy Li" position="Co-VP Communications" field="U1 Neuroscience" className="vp-border"/>
-                    <Member img="2024_members/Kathleen.jpg" n="Kathleen Brown" position="Co-VP Communications" field="U3 Anatomy and Cell Biology" className="vp-border"/>
-                    <Member img="2024_members/Lisa.png" n="Lisa Kawwas" position="Communications Coordinator" field="U1 Pharmacology and Therapeutics" className="non-vp-border"/>
+                    <Member img="2025_members/Cindy.jpg" n="Cindy Li" position="VP Communications" field="U2 Neuroscience" className="vp-border"/>
+                    <Member img="2025_members/Ryan_Mazaheri.JPG" n="Ryan Mazaheri" position="Communications committee" field="U2 Pharmacology" className="non-vp-border"/>
+                    <Member img="2025_members/Lisa.jpg" n="Lisa Kawwas" position="Communications Coordinator" field="U2 Pharmacology" className="non-vp-border"/>
                     </div>
                     <div className='m-committees'><span>Machine Learning</span></div> 
                     <div className='m-list'>
-                    <Member img="2024_members/Ozhan.png" n="Ozhan Dehghani" position="VP Machine Learning" field="M1 Neuro AI" className="vp-border"/>
-                    <Member img="2024_members/Leila.png" n="Leila Daoud" position="ML & Sponsorship Liason" field="U0 Mechanical Engineering" className="non-vp-border"/>
-                    <Member img="2024_members/Aymen.png" n="Aymen Boustani" position="Machine Learning" field="U4 Mechanical Engineering, Minor Applied AI"className="non-vp-border"/>
-                    <Member img="2024_members/Dev.png" n="Dev Patel" position="Machine Learning" field="Computer Science Graduate"className="non-vp-border"/>
+                    <Member img="2025_members/Theodor_Semerdzhiev.png" n="Theodor Semerdzhiev" position="VP Machine Learning" field="U3 Computer Science" className="vp-border"/>
+                    <Member img="2025_members/Dowoo_Kim.jpg" n="Dowoo Kim" position="ML & Sponsorship Liason" field="U3 Statistics/Computer Science" className="non-vp-border"/>
+                    <Member img="2025_members/Aymen.jpg" n="Aymen Boustani" position="Machine Learning" field="U4 Mechanical Engineering, Minor Applied AI"className="non-vp-border"/>
+                    <Member img="2025_members/Dev_Patel.jpg" n="Dev Patel" position="Machine Learning" field="Computer Science Graduate"className="non-vp-border"/>
                     </div>
                     <div className='m-committees'><span>Web Development</span></div> 
                     <div className='m-list'>
-                    <Member img="2024_members/Onur_Gul.png" n="Onur Gul" position="VP Web Development" field="U2 Mathematics/Computer Science" className ="vp-border"/>
+                    <Member img="2025_members/Saif_Shaikh.jpg" n="Saif Shaikh" position="VP Web Development" field="U1 Mathematics/Computer Science" className ="vp-border"/>
                     </div>
                 </div>
             </div>
